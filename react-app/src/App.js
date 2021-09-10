f44a95dbd7401af5100eb921a1569e587e8723a9
Nishadi <nishadidezoysa9@gmail.com>
2021-09-10 23:55:58 +0530

    add expenseitem.css

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,12 @@
 
-import './App.css';
+// import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div>
      <h2>Let's get started!</h2>
-    <p>This is also visible</p>
+    <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ react-app/src/components/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem(){
+    return <div>
+        <div>March 28th 2021</div>
+        <div>
+        <h2>Car Insuarance</h2>
+        </div>
+        <div>$201.12</div>
+    </div> 
+}
+export default ExpenseItem
