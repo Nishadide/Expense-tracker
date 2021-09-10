1a671d6973745ced4e5e9254023695672de8c896
Nishadi <nishadidezoysa9@gmail.com>
2021-09-10 23:59:46 +0530

    add expenseitem css classes

--- react-app/src/components/ExpenseItem.js
+++ react-app/src/components/ExpenseItem.js
@@ -1,10 +1,10 @@
 function ExpenseItem(){
-    return <div>
-        <div>March 28th 2021</div>
+    return <div className="expense-item"> 
+        <div className="expense-item__description">March 28th 2021</div>
         <div>
         <h2>Car Insuarance</h2>
         </div>
-        <div>$201.12</div>
+        <div className="expense-item__price">$201.12</div>
     </div> 
 }
 export default ExpenseItem
