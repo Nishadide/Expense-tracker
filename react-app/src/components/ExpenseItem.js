93a8703fda6865326d1b31f7ff37a92a6289ef31
Nishadi <nishadidezoysa9@gmail.com>
2021-09-13 23:59:24 +0530

    split date into seperate component

--- react-app/src/components/ExpenseItem.js
+++ react-app/src/components/ExpenseItem.js
@@ -1,17 +1,15 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 function ExpenseItem(props){
     // const expenseDate = new Date(2021, 2, 28);
     // const expenseTitle = 'Car Insuarance';
     // const expenseAmount = 294.23;  
-    const month = props.date.toLocaleString('en-US', {month: 'long'});
-    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-    const year = props.date.getFullYear();
+   
 
     return <div className="expense-item"> 
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
+        
         {/* <div className="expense-item__description">{props.date.toISOString()}</div> */}
+       <ExpenseDate data={props.date} />
         <div>
         <h2>{props.title}</h2>
         </div>
