b1ee38bf758ab73b30788e631d719336b9d788db
Nishadi <nishadidezoysa9@gmail.com>
2021-09-13 16:12:15 +0530

    remove hard coded text in expenseItem.js

--- react-app/src/components/ExpenseItem.js
+++ react-app/src/components/ExpenseItem.js
@@ -1,10 +1,15 @@
+import './ExpenseItem.css'
 function ExpenseItem(){
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insuarance';
+    const expenseAmount = 294.23;  
+
     return <div className="expense-item"> 
-        <div className="expense-item__description">March 28th 2021</div>
+        <div className="expense-item__description">{expenseDate.toISOString()}</div>
         <div>
-        <h2>Car Insuarance</h2>
+        <h2>{expenseTitle}</h2>
         </div>
-        <div className="expense-item__price">$201.12</div>
+        <div className="expense-item__price">${expenseAmount}</div>
     </div> 
 }
-export default ExpenseItem
+export default ExpenseItem;
