593c05e2584b82e397b979a266ac2a5eb74d8c26
Nishadi <nishadidezoysa9@gmail.com>
2021-09-14 18:08:34 +0530

    composition of card component with expenses using children props

--- react-app/src/components/ExpenseItem.js
+++ react-app/src/components/ExpenseItem.js
@@ -1,19 +1,23 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
+import Card from './Card';
 function ExpenseItem(props){
     // const expenseDate = new Date(2021, 2, 28);
     // const expenseTitle = 'Car Insuarance';
     // const expenseAmount = 294.23;  
    
 
-    return <div className="expense-item"> 
+    return (
+        <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+         <div  className='expense-item__description'>
+         <h2>{props.title}</h2>
+         </div>
+         <div className="expense-item__price">${props.amount}</div>
+         </Card>
+    ) 
         
-        {/* <div className="expense-item__description">{props.date.toISOString()}</div> */}
-       <ExpenseDate date={props.date} />
-        <div>
-        <h2>{props.title}</h2>
-        </div>
-        <div className="expense-item__price">${props.amount}</div>
-    </div> 
+ 
+ 
 }
 export default ExpenseItem;
